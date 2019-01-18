6b91f9dbdd7497273be748aa1d9f010999b86efd
Matthew Foxwell <foxwellmw@gmail.com>
2019-01-18 15:29:20 -0700

    Updates

---
+++ precache-manifest.352fb0c63c81f8bcf0c8ab4df565c6ef.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "88b21ef496e8492c64a1a43aa141c476",
+    "url": "/memoize/static/media/viewF.88b21ef4.gif"
+  },
+  {
+    "revision": "a6cbf59d144f96eae2505bc9cba05b67",
+    "url": "/memoize/static/media/view.a6cbf59d.gif"
+  },
+  {
+    "revision": "aa388c454c8718f8a369d1a68200fe2a",
+    "url": "/memoize/static/media/saveF.aa388c45.gif"
+  },
+  {
+    "revision": "1a418a8cbff2e33a9fcc",
+    "url": "/memoize/static/js/runtime~main.1a418a8c.js"
+  },
+  {
+    "revision": "b16954c47e38d53e9f39",
+    "url": "/memoize/static/js/main.b16954c4.chunk.js"
+  },
+  {
+    "revision": "3ab1e9f78bdbd0d7fa61",
+    "url": "/memoize/static/js/1.3ab1e9f7.chunk.js"
+  },
+  {
+    "revision": "b16954c47e38d53e9f39",
+    "url": "/memoize/static/css/main.827ede0d.chunk.css"
+  },
+  {
+    "revision": "3ab1e9f78bdbd0d7fa61",
+    "url": "/memoize/static/css/1.4b423748.chunk.css"
+  },
+  {
+    "revision": "9e2e6c4f913ca483e2aac7e402f12a37",
+    "url": "/memoize/index.html"
+  }
+];
